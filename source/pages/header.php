17564b20e7fbc6826546b02399344f50d780459f
Connor Krupp <connorkrupp@icloud.com>
2015-09-27 13:56:57 -0400

    Added basic header styles

--- source/pages/header.php
+++ source/pages/header.php
@@ -45,8 +45,9 @@
       <header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
         <div id="inner-header" class="wrap cf">
           <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-          <p id="logo" class="logo h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
+          <div id="logo">
+            <a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/css/images/logo.png"/></a>
+          </div>
           <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
             <?php wp_nav_menu(array(
               'container' => false,                           // remove nav container
